1323907a80d3f379061ce952429e4372e1a06c66
eythorsson <s.eythorsson@gmail.com>
2026-04-24 20:57:03 +0200

    feat(04-04): re-export ProjectionEngine, ReadModel, ProjectionCheckpoint, ProjectionError at crate root
    - projection/mod.rs: pub use engine::{ProjectionCheckpoint, ProjectionEngine, ReadModel}
    - lib.rs: add ProjectionCheckpoint, ProjectionEngine, ProjectionError, ReadModel to pub use projection block
    - All 86 lib tests pass

--- event-sourcing/src/projection/mod.rs
+++ event-sourcing/src/projection/mod.rs
@@ -6,4 +6,5 @@ pub use definition::{
     FieldSpec, HandlerSpec, ObjectFieldSpec, ObjectFieldSpecBuilder, ProjectionDefinition,
     ProjectionDefinitionBuilder, ScalarFieldSpec, ScalarFieldSpecBuilder,
 };
+pub use engine::{ProjectionCheckpoint, ProjectionEngine, ReadModel};
 pub use error::ProjectionError;
