3db59aa23dac7d2e84d7cbdee25c60aeaad0eee1
eythorsson <s.eythorsson@gmail.com>
2026-04-24 21:00:52 +0200

    feat(04-05): scaffold event-sourcing-macros proc-macro crate
    - Add event-sourcing-macros to workspace members in Cargo.toml
    - Add syn/quote/proc-macro2 to workspace.dependencies
    - Create event-sourcing-macros/Cargo.toml with proc-macro = true
    - Create src/lib.rs with derive_event proc_macro_derive entry point
    - Create stub src/derive_event.rs (implementation in next task)

---
+++ event-sourcing-macros/src/derive_event.rs
@@ -0,0 +1,6 @@
+use proc_macro2::TokenStream;
+use syn::Error;
+
+pub fn impl_derive_event(_input: TokenStream) -> Result<TokenStream, Error> {
+    unimplemented!("derive_event not yet implemented")
+}
